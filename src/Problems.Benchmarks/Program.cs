a26ec066b451dae597ebf16bc86b39a6ec442b32
furyslav <yaboytsov@ocs.ru>
2024-08-09 10:49:10 +0300

    init

---
+++ src/Problems.Benchmarks/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+//1 Microsecond (0.000001 sec)
+
+
+public class Setup
+{
+    static void Main(string[] args)
+        => BenchmarkSwitcher.FromAssembly(typeof(Setup).Assembly).Run(args);
+}
+
+
+
+
+
---
+++ src/Problems.Benchmarks/UniqueWords/UniqueWordsBenchmark.cs
@@ -0,0 +1,74 @@
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Problems.UniqueWords;
+
+namespace Problems.Benchmarks.UniqueWords;
+
+[SimpleJob(RuntimeMoniker.Net70)]
+[JsonExporter]
+[MemoryDiagnoser]
+public class UniqueWordsBenchmark
+{
+    [Params(1024, 1024 * 1024, 1024 * 1024 * 5)]
+    public int SourceLength { get; set; }
+
+    private string _source;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var bufffer = new StringBuilder(SourceLength);
+        var wordBuffer = new StringBuilder();
+        var random = new Random();
+        var repeatWords = new Stack<string>();
+
+        var remainderLength = SourceLength;
+        while (remainderLength > 0)
+        {
+            if (repeatWords.Count > 10)
+            {
+                while (repeatWords.Count > 0 && remainderLength > 0)
+                {
+                    var word = repeatWords.Pop();
+                    if (remainderLength - word.Length - 1 <= 0)
+                        continue;
+
+                    bufffer.Append(word);
+                    bufffer.Append(' ');
+
+                    remainderLength -= word.Length + 1;
+                }
+
+                continue;
+            }
+
+            const int min = 1;
+            var max = remainderLength > 100 ? 100 : remainderLength;
+            var wordLength = random.Next(min, max);
+            remainderLength -= wordLength + 1;
+
+            for (var index = 0; index < wordLength; index++)
+            {
+                var (minChar, maxChar) = index % 2 == 0 ? ('a', 'z') : ((int) 'A', (int) 'Z');
+                wordBuffer.Append((char) random.Next(minChar, maxChar));
+            }
+
+            bufffer.Append(wordBuffer);
+            bufffer.Append(' ');
+
+            repeatWords.Push(wordBuffer.ToString());
+            wordBuffer.Clear();
+        }
+
+        _source = bufffer.ToString();
+
+        Console.WriteLine(_source[..1024]);
+        Console.WriteLine($"Source length: {_source.Length}");
+    }
+
+    [Benchmark]
+    public void Test_Template() => UniqueWordsTemplate.CountUniqueWords(_source);
+}
+
+// dotnet run --project src/Problems.Benchmarks/Problems.Benchmarks.csproj -c Release --filter '*UniqueWordsBenchmark*' 
